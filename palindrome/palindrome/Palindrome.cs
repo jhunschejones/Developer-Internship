b83c50313becc2722033a88cb71bec6eb35cc1fc
ndykman <nathan.dykman@providence.org>
2018-03-15 17:30:57 -0700

    More cleanup and added the solution for review.

--- palindrome/palindrome/Palindrome.cs
+++ palindrome/palindrome/Palindrome.cs
@@ -1,66 +1,53 @@
 ﻿using System;
 
-namespace Palindrome // NOTE: Namespaces in C# are PascalCased (starting with a captial letter)
-{
+// Note, this is my solution. Please feel free to critique and ask questions.
+
+namespace Palindrome
+{ 
     public class PalindromeChecker
     {
-        // NOTE: XML comments can be used, just be aware.
-
-        /// <summary>
-        /// Checks if a string is a palindrome.
-        /// </summary>
-        /// <param name="myString"></param>
-        /// <returns></returns>
-
-
-        // I am calling this function to reverse the string without using arrays
-        public string ReverseString(string input)
-        {
-            var Reversed = "";
-
-            foreach (char c in input)
-            {
-                Reversed = c.ToString() + Reversed;
-            }
-
-            return Reversed;
-        }
-
         public bool IsPalindrome(string myString)
         { 
-            // NOTE, Most C# code uses BSD style, in which braces for a new function are on a new line.
-            // This is different than Javascript. 
-
-            // Null strings are not palindrome.
+            // Null strings are not a palindrome.
             if (myString == null)
             {
                 return false;
             }
 
-            // NOTE: This line has a problem: if (string.IsNullOrEmpty(myString) && myString != "")
-            // If string.IsNullOrEmpty is true, then myString != "" is always false. 
-            // We already checked for null, here, so we can just go to the next case. 
-
-            // Empty and strings of one character are always palindromes.
+            // Empty string and strings of one character are always palindromes.
             if (myString.Length < 2)
             {
                 return true;
             }
 
-            // NOTE: It is worth try to code without using reverse (or writing reverse yourself).
+            // Let us try to prove that this is *not* a palindrome. 
+            // If we fail, it must be a palindrome. 
+            
+            // This is a common pattern, to prove the opposite assertion and by failing to 
+            // do so, confirm the assertion is true. 
 
-            // convert string to array, reverse, convert back to string
-            //char[] myArray = myString.ToCharArray();
-            //Array.Reverse(myArray);
-            //string myStringReverse = new string(myArray);
+            // Integer division is fine here, even with the round down.
+            // We only need to check (about) half of the string to prove it is not
+            // a palindrome.
 
+            for (int i = 0; i < myString.Length / 2; i++)
+            {
+                // If the ith character does not match the Length - i - 1 character, 
+                // we don't have a palindrome. Just return false.
+               
+                if (myString[i] != myString[myString.Length - i - 1])
+                {
+                    return false;
+                }
+            }
 
+            // Take some time to analyze how the above works on paper. 
+            // Pick a string, and track the values of i, myString.Length - i - 1 
+            // and look at the pattern. It seems odd to try to do what a computer
+            // will do, but it is a critical skill. 
 
-            // Note: We can just return of the comparison here. 
-            // If they are equal, the string is a palindrome, if not, the string is not. 
-
-            return myString == ReverseString(myString);
+            // If we made it this far, we must have a palindrome.
+            return true;
         }
-
     }
 }
